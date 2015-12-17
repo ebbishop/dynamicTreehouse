3d2f54023fabffe3618af255bc12e42dcf42fc65
Emma Bishop <emma.b.bishop@gmail.com>
2015-12-16 23:05:58 -0500

    Modified router to handle post data

--- router.js
+++ router.js
@@ -1,19 +1,33 @@
 var Profile = require("./profile.js");
 var render = require('./render.js')
+var querystring = require('querystring');
+
+var commonHeader = { 'Content-Type': 'text/html' };
 
 // Handle HTTP route GET / and POST / (Home)
 function home(request, response){
 	// if (url = '/' && GET)
 		// show search
 	if(request.url === '/'){
-		response.writeHead(200, { 'Content-Type': 'text/plain' });
-		render.view('header', {},response);
-		render.view('search', {}, response);
-		render.view('footer', {}, response);
-		response.end();
+	  if(request.method.toLowerCase() === 'get'){
+  		response.writeHead(200, commonHeader);
+  		render.view('header', {},response);
+  		render.view('search', {}, response);
+  		render.view('footer', {}, response);
+  		response.end();
+	  }else{
+		// if (url = '/' && POST)
+		// get post data from body (uses name & value of "POST" thing)
+		request.on('data', function(postBody){
+			var query = querystring.parse(postBody.toString());
+			response.writeHead(303, {'Location': '/' + query.username});
+			response.end();
+		});
+		// extract username
+		// redirect to /username
+	  	
+	  }
 	}
-	// if (url = '/' && POST)
-		// redirect to /:username
 }
 // Handle HTTP route GET /:username (emmabbishop)
 function user(request, response){
@@ -21,7 +35,7 @@ function user(request, response){
 	// if (url == '/...')
 	var username = request.url.replace('/','');
 	if(username.length>0){
-		response.writeHead(200, { 'Content-Type': 'text/plain' });	
+		response.writeHead(200, commonHeader);	
 		render.view('header', {},response);
 
 		// get json from Treehouse (from profile.js module)
@@ -48,7 +62,7 @@ function user(request, response){
 		// on 'error'
 		studentProfile.on('error', function(error){
 			// show error
-			response.writeHead(200, { 'Content-Type': 'text/plain' });
+			response.writeHead(200, commonHeader);
 			render.view('error', {errorMessage: error.message}, response);
 			render.view('search', {}, response);
 			render.view('footer', {}, response);
